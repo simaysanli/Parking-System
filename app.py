12d439e8a60fca55850661ed7f518438ab40b96e
ge59dat <Hvlyildizsim95>
2021-01-17 19:55:03 +0300

    Add validity control for plates and write to database

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import exc
+import re
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -40,6 +42,35 @@ def check_request_validity(coming_request):
 
     if plate_info is None:
         return jsonify({"msg": "Missing field (plate) in JSON request"}), 400
+
+    if check_plate_format_validity(plate_info) is None:
+        return jsonify({"msg": "the " + coming_request.json['plate'] + " is not a valid German plate"}), 422
+
+    if db.session.query(Car.plate).filter_by(plate=plate_info).scalar() is not None:
+        return jsonify({"msg": "plate is already registered"}), 500
+
+    current_car = Car(plate_info)
+
+    if write_to_database(current_car):
+        return jsonify({"msg": "the " + coming_request.json['plate'] + " is a valid German plate"}), 200
+    else:
+        return jsonify({"msg": "internal error occurred"}), 502
+
+
+def write_to_database(car):
+    try:
+        db.session.add(car)
+        db.session.commit()
+        return True
+    except exc.SQLAlchemyError as e:
+        db.session.rollback()
+        db.session.flush()
+        return False
+
+
+def check_plate_format_validity(plate):
+    valid_plate = re.match(r'^[A-ZÄÖÜß]{1,3}-[aA-ZÄÖÜß]{1,2}[1-9]{0,4}$', plate, re.M | re.I)
+    return valid_plate
 
 
 if __name__ == '__main__':
